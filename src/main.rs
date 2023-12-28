46afba817e02fcbc326dce60105f92d893b1b4ad
Simple <huyang1994822@gmail.com>
2023-12-28 17:26:36 +0800

    add sql

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() {
     let ho = String::from("Hello, world!");
     println!("println: \n{}", ho);
     println!();
-    assert_eq!(ret_unit_type(), ())
+    assert_eq!(ret_unit_type(1), ());
+    assert_eq!(ret_unit_type(2), ());
 }
 
-fn ret_unit_type() {
-    let x = 1;
+fn ret_unit_type(x: i32) {
     // if 语句块也是一个表达式，因此可以用于赋值，也可以直接返回
     // 类似三元运算符，在Rust里我们可以这样写
     let y = if x % 2 == 1 {
