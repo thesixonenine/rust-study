5423b6de9517946b254183393a5d5b8b6e32a488
Simple <huyang1994822@gmail.com>
2023-12-28 17:09:44 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,31 @@ fn main() {
     let ho = String::from("Hello, world!");
     println!("println: \n{}", ho);
     println!();
+    assert_eq!(ret_unit_type(), ())
+}
+
+fn ret_unit_type() {
+    let x = 1;
+    // if 语句块也是一个表达式，因此可以用于赋值，也可以直接返回
+    // 类似三元运算符，在Rust里我们可以这样写
+    let y = if x % 2 == 1 {
+        "odd"
+    } else {
+        "even"
+    };
+    // 或者写成一行
+    let z = if x % 2 == 1 { "odd" } else { "even" };
+    println!("{}", x);
+    println!("{}", y);
+    println!("{}", z);
+    // 表达式如果不返回任何值, 会隐式地返回一个()
 }
 
 #[allow(dead_code)]
 fn range() {
-  for i in 1..=5 {
-    println!("{}",i);
-  }
+    for i in 1..=5 {
+        println!("{}", i);
+    }
 }
 
 #[allow(dead_code)]
