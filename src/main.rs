4e743df1b5f2fc15701f89429dff5885d4aa36ab
Simple <huyang1994822@163.com>
2025-02-04 21:31:40 +0800

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,7 @@ fn main() {
 fn ret_unit_type(x: i32) {
     // if 语句块也是一个表达式，因此可以用于赋值，也可以直接返回
     // 类似三元运算符，在Rust里我们可以这样写
-    let y = if x % 2 == 1 {
-        "odd"
-    } else {
-        "even"
-    };
+    let y = if x % 2 == 1 { "odd" } else { "even" };
     // 或者写成一行
     let z = if x % 2 == 1 { "odd" } else { "even" };
     println!("{}", x);
